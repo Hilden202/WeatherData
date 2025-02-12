2539906a3759cd8cc910a52efd469613f33d8b95
David Rantanen <david.rantanen@telia.com>
2025-02-12 11:02:56 +0100

    Test commit

--- Data/ReadFile.cs
+++ Data/ReadFile.cs
@@ -50,7 +50,7 @@ namespace WeatherData.Data
                     {
                         if (!Regex.IsMatch(line, @"^(2016-(0[6-9]|1[0-2])|2016-05-(3[1])|2017-01-(0[1-9]|10))\s"))
                         {
-                            Regex regex = new Regex(@"(\d{4})-(\d{2})-(\d{2}))")
+                            Regex regex = new Regex(@"(\d{4})-(\d{2})-(\d{2})");
                             
 
 
@@ -59,35 +59,36 @@ namespace WeatherData.Data
 
                             if (match.Success)
                             {
-                                string year = match.Groups[1].Value;
+                                string year = match.Groups[1].Value; // Om vi ska räkna flera år
                                 string month = match.Groups[2].Value;
                                 string day = match.Groups[3].Value;
-                                
+
                                 float temperature = ExtractTemperature(line);
                                 int humidity = ExtractHumidity(line);
                             
-                            if (currentDate == null)
-                            {
+                                if (currentDate == null)
+                                {
                                     currentDate = date;
-                            }
+                                }
 
-                            if (date != currentDate)
-                            {
-                                double aveDayTemp = sumTemperature / count;
-                                double aveDayHumidity = humidity / count;
+                                if (date != currentDate)
+                                {
+                                    double aveDayTemp = sumTemperature / count;
+                                    double aveDayHumidity = humidity / count;
 
-                                sumDayHumidity += aveDayHumidity;
-                                sumDayTemp += aveDayTemp;
+                                    sumDayHumidity += aveDayHumidity;
+                                    sumDayTemp += aveDayTemp;
 
-                                currrentDate = date;
-                                sumTemperature = 0;
-                                sumHumidity = 0;
-                                count = 0;
+                                    currrentDate = date;
+                                    sumTemperature = 0;
+                                    sumHumidity = 0;
+                                    count = 0;
 
-                                 double moldRisk = (aveDayHumidity - 78) * (aveDayTemp / 15) / 0.22;
+                                    double moldRisk = (aveDayHumidity - 78) * (aveDayTemp / 15) / 0.22;
 
-                                string riskLevel = moldRisk > 59 ? "High Risk of mold" : "Low Risk of mold";
-                            }
+                                    string riskLevel = moldRisk > 59 ? "High Risk of mold" : "Low Risk of mold";
+                                }
+                                
                             sumTemperature += temp;
                             sumHumidity += humidity;
                             count ++;
